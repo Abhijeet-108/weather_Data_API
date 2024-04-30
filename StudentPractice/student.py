b92634d5e44023381ffc47a6057a8a8bec552197
Asus <abhijeet.jmt108@gmail.com>
2024-04-30 23:17:32 +0530

    shows stattion table and id

--- StudentPractice/student.py
+++ StudentPractice/student.py
@@ -1,17 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 import pandas as pd
 
 app = Flask(__name__)
 df = pd.read_csv("dictionary.csv")
+
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/api/v1/<words>/")
 def api(words):
     defination = df.loc[df['word'] == words]['definition'].squeeze()
-    result = {'words':words,'defination': defination}
+    result = {'words': words, 'defination': defination}
     return result
+
 
 if __name__ == '__main__':
     app.run(debug=True)
